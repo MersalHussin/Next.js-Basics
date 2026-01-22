aab7d71906d85404533ace2ce81d2e84d7ab0a64
mersalhussin <mersalhussin@gmail.com>
2026-01-22 13:55:37 +0200

    Data Fetching with Server Component

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,17 +1,24 @@
 interface IProps{
-
 params: {
-    id: string;
+    id: number;
  }
-
 }
-
+async function getProducts(id:number){
+    const data = await fetch(`https://dummyjson.com/products/${id}`)
+    const products = await data.json()
+    return products
+}
 
  const page = async ({ params }: IProps ) => {
      const {id} = await params
+     const {title , thumbnail } = await getProducts(id)
     //  console.log(id);
     return (
-        <div>Product ID - {id} </div>
+        <>
+        <h3>Product ID - {id} </h3>
+        <h1> {title} </h1>
+        <img src={thumbnail} alt={title} />
+        </>
     );
 }
 
