72beae8540deb428eec83134d1e2742ba86fb758
mersalhussin <mersalhussin@gmail.com>
2026-01-25 10:05:37 +0200

    Open Graph

--- app/(Shopping)/products/[id]/page.tsx
+++ app/(Shopping)/products/[id]/page.tsx
@@ -3,6 +3,34 @@ import ProductDetails from "@/app/components/ProductDetails";
 import { Suspense } from "react";
 import ProductDetailsSkeleton from "@/app/components/ProductDetailsSkeleton";
 
+import type { Metadata } from 'next'
+ 
+type Props = {
+  params: Promise<{ id: number }>
+  searchParams: Promise<{ [key: string]: string | string[] | undefined }>
+}
+ 
+export async function generateMetadata(
+  { params }: Props,
+): Promise<Metadata> {
+  const id = (await params).id
+ 
+  // fetch post information
+  const {title, description , thumbnail} = await fetch(`https://dummyjson.com/products/${id}`).then((res) =>
+    res.json()
+  )
+ 
+  return {
+   title,
+   description,
+   openGraph:{
+    images: thumbnail
+   }
+
+  }
+}
+
+
 interface IProps{
 params: {
     id: number;
